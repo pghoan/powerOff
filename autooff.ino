bd0ec8c5528aa97a8a57050efac008e5f244ae05
Hoan <pghoan@yahoo.com>
2025-11-18 15:46:14 +0700

    Them thu vien doc dong dien

--- autooff.ino
+++ autooff.ino
@@ -1,16 +1,28 @@
 #include "AverageOverTime.h"
+#include "EmonLib.h"                   // Include Emon Library
 #define I_MIN  100.0  // ma
 #define TIMEOUT 15 // minutes
 AverageOverTime avg(10);   // Trung bình trong 10 giây
+EnergyMonitor emon1;                   // Create an instance
+int CT_pin = 5; //  CT sensor pin connected to A5 pin of Arduino
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   avg.begin();             // Bắt đầu đếm 10 giây
+  emon1.current(CT_pin, 32.59);             // Current: input pin, calibration.
 }
 
 void loop() {
   static uint32_t last = 0;
   static uint32_t last_imin = 0;
+
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  
+  Serial.print(Irms*230.0);         // Apparent power
+  Serial.print(" ");
+  Serial.println(Irms);          // Irms
+
+  // doc gia tri cam bien
   if (millis() - last >= 2) {   // Lấy mẫu mỗi 2ms (tùy ý)
     last = millis();
 
