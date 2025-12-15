92aeeb360837c582a830bbc74353562cc819baa9
pghoan <pghoan@yahoo.com>
2025-12-15 17:41:25 +0700

    Update autooff.ino

--- autooff.ino
+++ autooff.ino
@@ -10,6 +10,12 @@ int CT_pin = A2; //  CT sensor pin connected to A5 pin of Arduino
 
 #include <avr/sleep.h>
 #include <avr/power.h>
+// https://github.com/thomasfredericks/Bounce2
+#include <Bounce2.h>
+
+// INSTANTIATE A Button OBJECT FROM THE Bounce2 NAMESPACE
+Bounce2::Button button = Bounce2::Button();
+
 
 const byte BUTTON_PIN = 2;  // D2 = INT0
 //const unsigned long AWAKE_TIME_MS = 15UL * 60UL * 1000UL; // 15 phút
@@ -28,6 +34,9 @@ void wakeISR() {
 
 // Hàm đưa Arduino vào sleep, chỉ dậy khi bấm nút
 void goToSleep() {
+  ledState = false;
+  digitalWrite(ledPin, ledState); // tat ro le
+  digitalWrite(controlPin, ledState); // tat ro le
   delay(1000);
   wokeByButton = false;
 
@@ -50,6 +59,7 @@ void goToSleep() {
   EIFR = bit(INTF0);
   interrupts();
 
+
   // ------ Bắt đầu ngủ ------
   sleep_cpu();
   // ------ Tỉnh dậy ở đây (sau khi bấm nút) ------
@@ -62,6 +72,11 @@ void goToSleep() {
 
   // Bắt đầu đo thời gian hoạt động sau khi thức dậy
   startMillis = millis();
+  // ← KHI THỨC DẬY SẼ CHẠY TIẾP DÒNG TIẾP THEO, KHÔNG NHẢY LẠI setup()
+  ledState = true;
+  digitalWrite(ledPin, ledState); // bat ro le
+  digitalWrite(controlPin, ledState); // tat ro le
+  delay(5000); // thoi gian bat it nhat 5s
 }
 
 void setup() {
@@ -70,19 +85,24 @@ void setup() {
   Serial.begin(9600);
   avg.begin();             // Bắt đầu đếm 10 giây
   emon1.current(CT_pin, 0.125);            // Current: input pin, calibration.
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  //pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   pinMode(controlPin, OUTPUT);
   //digitalWrite(ledPin, ledState); // off led
+  // 1) IF YOUR BUTTON HAS AN INTERNAL PULL-UP
+  button.attach( BUTTON_PIN ,  INPUT_PULLUP ); // USE INTERNAL PULL-UP
+
+  // DEBOUNCE INTERVAL IN MILLISECONDS
+  button.interval(5);
+
+  // INDICATE THAT THE LOW STATE CORRESPONDS TO PHYSICALLY PRESSING THE BUTTON
+  button.setPressedState(LOW);
 
   // Nếu cần debug:
   // Serial.begin(9600);
   Serial.println("Booting, going to sleep...");
   // Ngay khi bật nguồn sẽ ngủ luôn, chờ bấm nút để dậy
   goToSleep();
-  ledState = true;
-  digitalWrite(ledPin, ledState); // bat ro le
-  digitalWrite(controlPin, ledState); // tat ro le
 }
 
 void loop() {
@@ -96,22 +116,22 @@ void loop() {
     float Irms = emon1.calcIrms(1480);  // Calculate Irms only
     avg.addValue(Irms);
   }
-
+  // UPDATE THE BUTTON
+  // YOU MUST CALL THIS EVERY LOOP
+  button.update();
+  if (button.pressed()) {
+    Serial.println("Button pressed...");
+    goToSleep();  // sẽ chỉ dậy khi bấm nút lần nữa
+    last_imin = millis();
+  }
   // Khi đủ 10 giây → lấy kết quả và xử lý
   if (avg.isFinished()) {
     float ket_qua = avg.getAverage();
     Serial.println(ket_qua);          // Irms
     if (ket_qua < I_MIN) {
-      if (millis() - last_imin >= TIMEOUT_MS) {
-        ledState = false;
-        digitalWrite(ledPin, ledState); // tat ro le
-        digitalWrite(controlPin, ledState); // tat ro le
+      if (millis() - last_imin >= TIMEOUT_MS  ) { // timout or bam phim --> ngu
         goToSleep();  // sẽ chỉ dậy khi bấm nút lần nữa
-        // ← KHI THỨC DẬY SẼ CHẠY TIẾP DÒNG TIẾP THEO, KHÔNG NHẢY LẠI setup()
         last_imin = millis();
-        ledState = true;
-        digitalWrite(ledPin, ledState); // bat ro le
-        digitalWrite(controlPin, ledState); // tat ro le
       }
     }
     else { // co tai
